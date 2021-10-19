c3bcf85fc75a50154e39862f1f6d867a1cd0fd3e
Maxime Valencik <valencikmaxime@gmail.com>
2021-10-18 21:23:42 -0400

    add files

---
+++ APINasa.js
@@ -0,0 +1,12 @@
+
+async function getImage(){
+    const image = await axios.get(" https://api.nasa.gov/planetary/apod?api_key=CT0rIlDfBRbdwXlYVIUoEN1VdYMh7iDO0G9wj7I8");
+    const img = document.createElement('img');
+    img.src = image.data.hdurl;
+    const container = document.querySelector('#img');
+    container.append(img);
+}
+
+const button = document.querySelector('button');
+button.addEventListener('click', getImage);
+
